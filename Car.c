e253d883f99d19a03a670a2c697f4b26e8219936
Shruthi3192 <61211304+Shruthi3192@users.noreply.github.com>
2020-03-06 18:32:09 +0100

    Update Car.c

--- Car.c
+++ Car.c
@@ -44,30 +44,34 @@ void M2_reverse(unsigned char pwm)
 }
  
  
-// Motor Initialization routine -- this function must be called
-//  before you use any of the above functions
+
 void Port_init()
 {
+	//------------------ MOTOR CONTROL ----------------------------------------
     // configure for inverted PWM output on motor control pins:
     //  set OCxx on compare match, clear on timer overflow
     //  Timer0 and Timer2 count up from 0 to 255
-    TCCR0A = TCCR2A = 0xF3;
+    TCCR0A |=(1<<WGM00)|(1<<WGM01)|(1<<COM0A0)|(1<<COM1A1)|(1<<COM0B1)|(1<<COM0B0);
+	TCCR2A |=(1<<WGM20)|(1<<WGM21)|(1<<COM2A0)|(1<<COM2A1)|(1<<COM2B1)|(1<<COM2B0);
  
     // use the system clock/8 (=2.5 MHz) as the timer clock
-    TCCR0B = TCCR2B = 0x02;
+    TCCR0B |= (1<<CS01);
+	TCCR2B |=(1<<CS21);
  
     // initialize all PWMs to 0% duty cycle
-    OCR0A = OCR0B = OCR2A = OCR2B = 0;
+    OCR0A = 0;
+	OCR0B = 0;
+	OCR2A = 0;
+	OCR2B = 0;
  
-    // set PWM pins as digital outputs (the PWM signals will not
-    // appear on the lines if they are digital inputs)
     DDRD |= (1 << PORTD3) | (1 << PORTD5) | (1 << PORTD6);
     DDRB |= (1 << PORTB3);
 	
+	//------------------ ULTRASONIC SENSOR CONTROL ----------------------------------------
 	
-	DDRD|=(1<<PORTD0); //	Set  PORTD0 as ultrasonic trigger.
+	DDRD|=(1<<PORTD0); //	Set  PORTD0 as ultrasonic trigger(output). PORTD2/INT0 as ultrasonic echo
 		
-	EIMSK |= (1<<INT0); //enable external interrupt
+	EIMSK |= (1<<INT0);  //enable external interrupt
 	EICRA |= (1<<ISC00); // Any logical change on INT0 generates an interrupt request.
 
 	//Config and enable Timer1
@@ -77,85 +81,72 @@ void Port_init()
 
 
  
-
-void delay_ms(unsigned int time_ms)
-{
-    // _delay_ms() comes from <util/delay.h> and can only
-    //  delay for a max of around 13 ms when the system
-    //  clock is 20 MHz, so we define our own longer delay
-    //  routine based on _delay_ms()
- 
-    unsigned int i;
- 
-    for (i = 0; i < time_ms; i++)
-        _delay_ms(1);       
-}
-
-
  
 int main()
 {
 	Port_init();//Port Initialization
-	unsigned char sm;
-	sei();
-	
-   
+	DDRC = (1<<PORTC4)|(1<<PORTC5);// test led's to check interrupt working of ultrasonic sensor
+	sei(); //enabling global interrupt
+  
+  
 	while(1)
 	{	
-		DDRD &= ~(1 << PORTD1);     // Clear the PB0 pin
-		// PD1 (PCINT17 pin) is now an input
-
-		PORTB |= (1 << PORTD1);    // turn On the Pull-up
-		// PD1 is now an input with pull-up enabled
-
-
-		PCICR |= (1 << PCIE0);    // set PCIE0 to enable PCMSK0 scan
-		PCMSK0 |= (1 << PCINT17);  // set PCINT0 to trigger an interrupt on state change
-
-		
+				
+		if((PINC & (1<<PORTC2))) // if the IR receiver is not transmitting or seeing green traffic light ::: MOTORS ARE FUNCTIONING
+		{
+			_delay_ms( 100 );
+		 
+			 PORTD|=(1<<PIND0);       // trigger=1 high pulse for ultrasonic sensor
+			 _delay_us(10);           // wait minimum 10 us
+			 PORTD &=~(1<<PIND0);      // trigger=0 low pulse
+		 
+			pulse = 0;
+			while (pulse == 0) // to give time for the interrupt to be hit
+			{
+			}
+		 
+			Distance = pulse/58;      // take distance in cm  (speed of sound in air 29 microsecond per cm *2)
+			 if (Distance <=20)         // compare the value of count which is distance
+			{ 
+				//if distance of obstacle is 20cm or less stop the motors
+				M1_forward(0);
+				M2_forward(0);
+				PORTC=(1<<PORTC5);
+			
+			}
+			else
+			{
+				M1_forward(100);
+				M2_forward(100);
+				PORTC=(1<<PORTC4);
+			 }
+		}// end of if condition checking for IR receiver transmission
 	
-		 M1_forward(80);
-		 M2_forward(80);
-		 PORTD|=(1<<PIND0); // trigger=1 high pulse
-		 _delay_us(10); // wait minimum 10 us
-		 PORTD &=~(1<<PIND0); // trigger=0 low pulse
-		 Distance = pulse/58; // take distance in cm  (29 microsecond per cm *2)
-		 _delay_ms(20);
-		 if(Distance <=20) // compare the value of count which is distance
-		 {
+		else
+		{// if the IR receiver is  transmitting or seeing red traffic light ::: MOTORS ARE STOPPED
 			M1_forward(0);
 			M2_forward(0);
-		 }
-			 
-		_delay_ms( 10 );
-	}
-	
-	}
+		}
+	}// end of while
+}// end of main
 
 
-ISR(INT0_vect)
-{
-	if(i == 1)
-	{
-		TCCR1B = 0;
-		pulse = TCNT1;
-		TCNT1 = 0;
-		i = 0;
-	}
-
-	if(i==0)
+ISR(INT0_vect) 
+{ 
+	//PIND2 connected to echo, interrupt triggered on any change in logic of this pin
+	// if PIND2 on the rising edge
+	if((PIND & (1<<PORTD2)))
 	{
 		TCCR1B |= 1<<CS10;
-		i = 1;
+		TCNT1 = 0;
+	}
+
+	else
+	{
+		// if PIND2 on the falling edge
+		TCCR1B = 0;
+		pulse = TCNT1; // variable which keeps track of the amount of time it took for the echo to register the signal
+	
 	}
 }
 
-char check,data,invdata,count=0;
-
-ISR (PCINT0_vect)
-{
-	/* interrupt code here */
-	if 
-}
-
-	
